52a201c792097bf01ed08a5d6fcb7d076b66b581
Apurva Pimparkar <33530227+Apurvapimparkar@users.noreply.github.com>
2019-09-16 16:44:03 -0700

    Update settings.py

--- Cloud_Project_1/Cloud_Project_1/settings.py
+++ Cloud_Project_1/Cloud_Project_1/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Cloud_Project_1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hitme_db',
+	    'USER': 'hitme_user1',
+	    'PASSWORD': 'Mysql@123',
+	    'HOST': 'localhost',
+	    'PORT': '',
     }
 }
 
